9946e1d5c609c5a52f0f8f5471789b006c8ccbc8
Uyen Nguyen <uyen.carolyn.nguyen@gmail.com>
2026-06-03 23:06:57 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -138,43 +138,41 @@
 
 	// Set the date we're counting down to
 	//var countDownDate = new Date("Jul 17, 2027 12:00:00").getTime();
-	var countDownDate = new Date(2027, 7, 17).getTime(); 
+		// Set the date we're counting down to (Using standard text format to bypass the month bug)
+	var countDownDate = new Date("Jul 17, 2027 12:00:00").getTime();
 
 	// Update the count down every 1 second
 	var x = setInterval(function() {
 
-	// Get todays date and time
-	var now = new Date().getTime();
+		// Get todays date and time
+		var now = new Date().getTime();
 
-	// Find the distance between now an the count down date
-	var distance = countDownDate - now;
+		// Find the distance between now and the count down date
+		var distance = countDownDate - now;
 
-	// Time calculations for days, hours, minutes and seconds
-	var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-	var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-	var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-	var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+		// Time calculations for days, hours, minutes and seconds
+		var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+		var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+		var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+		var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-	// Display the result in an element with id="demo"
-	// document.getElementById("demo").innerHTML = days + "Days " + hours + "Hours "
-	// + minutes + "Minutes " + seconds + "Seconds ";
-
-	// Display the result in an element with id="demo"
-	document.getElementById("days").innerHTML = days +" <small>days</small>";
-	document.getElementById("hours").innerHTML = hours + " <small>hours</small> ";
-	document.getElementById("minutes").innerHTML = minutes + " <small>minutes</small> ";
-	document.getElementById("seconds").innerHTML = seconds + " <small>seconds</small> ";
-
-	// If the count down is finished, write some text 
-	if (distance < 0) {
-		clearInterval(x);
-		document.querySelector('.simply-days .simply-amount').innerHTML = "0";
-		document.querySelector('.simply-hours .simply-amount').innerHTML = "0";
-		document.querySelector('.simply-minutes .simply-amount').innerHTML = "0";
-		document.querySelector('.simply-seconds .simply-amount').innerHTML = "0";
-	}
+		// Check if the wedding date has already passed
+		if (distance < 0) {
+			clearInterval(x);
+			document.getElementById("days").innerHTML = "0 <small>days</small>";
+			document.getElementById("hours").innerHTML = "0 <small>hours</small>";
+			document.getElementById("minutes").innerHTML = "0 <small>minutes</small>";
+			document.getElementById("seconds").innerHTML = "0 <small>seconds</small>";
+		} else {
+			// Update the matching live display boxes on the webpage layout
+			document.getElementById("days").innerHTML = days + " <small>days</small>";
+			document.getElementById("hours").innerHTML = hours + " <small>hours</small>";
+			document.getElementById("minutes").innerHTML = minutes + " <small>minutes</small>";
+			document.getElementById("seconds").innerHTML = seconds + " <small>seconds</small>";
+		}
+		
 	}, 1000);
-
+	
 	// Document on load.
 
 	$(function(){
